5c3f0b60dc1e419e3cef599cf77acf41ec99eb3a
dzhimirikin <d.zhimirikin@gmail.com>
2026-06-10 21:08:43 +0300

    Create PhotoInfo class for photo metadata

---
+++ models/photo_info.dart
@@ -0,0 +1,11 @@
+class PhotoInfo {
+  final String fileName;
+  final DateTime date;
+  final String path;
+
+  PhotoInfo({
+    required this.fileName,
+    required this.date,
+    required this.path,
+  });
+}
