a725506cb39fe4bd65e445c741adb6b82b4485c9
dzhimirikin <d.zhimirikin@gmail.com>
2026-06-10 21:13:47 +0300

    Create home_screen.dart

---
+++ screens/home_screen.dart
@@ -0,0 +1,13 @@
+ElevatedButton(
+  onPressed: selectFolder,
+  child: const Text(
+    "Select Folder",
+  ),
+);
+
+ElevatedButton(
+  onPressed: upload,
+  child: const Text(
+    "Upload",
+  ),
+);
